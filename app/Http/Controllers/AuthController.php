dbf5c00f5aa0a05daf1e3ccbf9811ab6c269da6d
nourchomrong <nourchomrong.biu@gmail.com>
2026-07-10 15:48:11 +0700

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -367,7 +367,7 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        $request->user()?->currentAccessToken()?->delete();
+        $request->user()?->tokens()->delete();
         return response()->json(['message' => 'Logged out successfully']);
     }
 
